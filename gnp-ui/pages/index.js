cdb1d12ed87963f324a2b352501992f41561efbd
imnotshreyas <imnotshreyas@users.noreply.github.com>
2022-02-13 18:35:49 +0530

    Added nextjs and navbar

---
+++ gnp-ui/pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+    <Head> <title> Shreyas S - Product Designer</title></Head>
+    <div> Shreyas S Sreedhar</div>
+    </div>
+  )
+}
